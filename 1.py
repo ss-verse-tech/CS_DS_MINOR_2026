1f0226a21223909041c8fc3aa36d93be12a7a742
Sudhanshu <ersshekhar05@gmail.com>
2026-08-16 14:32:55 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,18 @@
+class Dog:
+    color = "Black"
+    eyes_color = "Brown"
+
+    def bark(self):
+        return f"{self.name} can bark "
+
+
+# d1 = Dog(); #instance
+# # print(d1);
+# # print(d1.color)
+# # print(d1.bark());
+# d1.bark()
+
+d1 = Dog();
+d2 = Dog();
+d3 = Dog();
+d4 = Dog();
---
+++ 2.py
@@ -0,0 +1,43 @@
+# class Bird:
+#     name = "Sparrow"
+#     wings = "black"
+#     color = "Brown"
+
+#     def fly(self):
+#         return f"{self.name} can fly"
+
+
+# sparrow = Bird()
+# print(sparrow.name)
+# print(sparrow.fly())
+
+# coockoo = Bird();
+# coockoo.name = "Cookoo"
+# print(coockoo.name)
+
+
+class Bird:
+    def __init__(self, name, color, wings_color):
+        self.name = name
+        self.color = color
+        self.wings_color = wings_color
+        print("Constructor is calling")
+
+    def fly(self):
+        return f"{self.name} can fly"
+
+
+sparrow = Bird("Sparrow", "Brown", "black")
+Cookoo = Bird("Coockoo", "black", "black")
+Pigon = Bird("Pigon", "white","white" )
+
+# print(sparrow.name)
+# print(sparrow.color)
+# print(sparrow.wings_color)
+# # print(sparrow.fly())
+# print(sparrow.fly())
+
+print(Cookoo.name)
+print(Cookoo.color)
+print(Cookoo.wings_color)
+
